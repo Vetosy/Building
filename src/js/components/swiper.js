0fe407016eaa11c3aa94259ac0829d6cb29990ad
Vitalii Stepnov <97784759+Vetosy@users.noreply.github.com>
2023-04-10 18:34:38 +0400

    added adout-page

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -94,3 +94,49 @@
       },
     });
   }
+
+
+  const historySlider = document.querySelector('.history-slider');
+
+  let history
+
+  if (historySlider) {
+    history = new Swiper(historySlider, {
+      spaceBetween: 20,
+      slidesPerView: 1,
+      navigation: {
+        nextEl: '.history__next',
+        prevEl: '.history__prev',
+      },
+    });
+  }
+
+
+  history.on('slideChange', function () {
+    console.log(history.realIndex)
+
+    historyBtns.forEach(el => {
+      el.classList.remove('history-nav__btn--active')
+    })
+
+    document.querySelector(`.history-nav__btn[data-index="${history.realIndex}"]`).classList.add('history-nav__btn--active')
+
+  })
+
+  const historyBtns = document.querySelectorAll('.history-nav__btn')
+
+  historyBtns.forEach((el, idx) => {
+    el.setAttribute('data-index', idx)
+
+    el.addEventListener('click', (e) => {
+      const index = e.currentTarget.dataset.index;
+
+      historyBtns.forEach(el => {
+        el.classList.remove('history-nav__btn--active')
+      })
+
+      e.currentTarget.classList.add('history-nav__btn--active')
+
+      history.slideTo(index);
+    })
+  })

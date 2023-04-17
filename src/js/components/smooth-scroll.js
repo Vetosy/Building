537c4fd531973e86f87146f8daa6cc0ed1d73b0e
Vitalii Stepnov <97784759+Vetosy@users.noreply.github.com>
2023-04-17 22:14:36 +0400

    added to-top

---
+++ src/js/components/smooth-scroll.js
@@ -0,0 +1,22 @@
+import SmoothScroll from 'smooth-scroll';
+
+const scroll = new SmoothScroll('.to-top', {
+  speed: 500
+});
+const toTop = document.querySelector('.to-top')
+const heroHeight = document.querySelector('.hero').offsetHeight;
+
+const isVisibleToTop = (y = 0) => {
+  if (y >= heroHeight) {
+    toTop.classList.add('to-top--active')
+  } else {
+    toTop.classList.remove('to-top--active')
+  }
+}
+
+isVisibleToTop(window.scrollY)
+
+window.addEventListener('scroll', () => {
+  let y = window.scrollY
+  isVisibleToTop(y)
+})

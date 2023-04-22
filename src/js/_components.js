856bf0450b28fb899218859eb747d26bdf3eebe5
Vitalii Stepnov <97784759+Vetosy@users.noreply.github.com>
2023-04-22 19:07:56 +0400

    Updates

--- src/js/_components.js
+++
@@ -1,9 +0,0 @@
-import './components/swiper';
-import './components/progress';
-import './components/accordion';
-import './components/portfolio-tabs';
-import './components/selects';
-import './components/video';
-import './components/smooth-scroll';
-import './components/modal';
-import './components/burger';
